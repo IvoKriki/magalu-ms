dc648b851fa665148edbf97a56cb6fde9ed3c53f
Ivo Kriki <ivo.kriki@outlook.com>
2024-07-11 15:19:34 +0100

    created schedueler

--- src/main/java/com/chalange/magalums/scheduler/AppTaskScheduler.java
+++ src/main/java/com/chalange/magalums/scheduler/AppTaskScheduler.java
@@ -1,5 +1,6 @@
 package com.chalange.magalums.scheduler;
 
+import com.chalange.magalums.service.NotificationService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.scheduling.annotation.Scheduled;
@@ -12,11 +13,17 @@ import java.util.concurrent.TimeUnit;
 public class AppTaskScheduler {
 
     private static final Logger log = LoggerFactory.getLogger(AppTaskScheduler.class);
+    private final NotificationService notificationService;
+
+    public AppTaskScheduler(NotificationService notificationService) {
+        this.notificationService = notificationService;
+    }
 
     @Scheduled(fixedDelay = 1L, timeUnit = TimeUnit.MINUTES)
     public void runTasks(){
         var dateTime = LocalDateTime.now();
         log.info("Running at {}", dateTime);
+        notificationService.checkStatusAndSend(dateTime);
     }
 
 }
--- src/main/java/com/chalange/magalums/service/NotificationService.java
+++ src/main/java/com/chalange/magalums/service/NotificationService.java
@@ -8,7 +8,10 @@ import lombok.AllArgsConstructor;
 import org.aspectj.apache.bcel.classfile.Module;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
+import java.util.List;
 import java.util.Optional;
+import java.util.function.Consumer;
 
 @Service
 
@@ -34,8 +37,24 @@ public class NotificationService {
         if (notification.isPresent()){
             notification.get().setStatus(Status.Values.CANCELLED.toStatus());
             notificationRepository.save(notification.get());
-
         }
+    }
+
+    public void checkStatusAndSend(LocalDateTime dateTime){
+        var notifications = notificationRepository.findByStatusInAndDateTimeBefore(
+                List.of(Status.Values.PENDING.toStatus(), Status.Values.ERROR.toStatus()),
+                dateTime
+        );
+        notifications.forEach(sendNotification());
+    }
+
+    private Consumer<Notification> sendNotification() {
+        return n -> {
+            //TODO: send notification
+
+            n.setStatus(Status.Values.SUCCESS.toStatus());
+            notificationRepository.save(n);
+        };
     }
 
 }
