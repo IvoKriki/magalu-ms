4556f1074aa9673c9cc9d8bf8479796cf81afb08
Ivo Kriki <ivo.kriki@outlook.com>
2024-07-11 01:22:13 +0100

    created cancel notification

--- src/main/java/com/chalange/magalums/controller/NotificationController.java
+++ src/main/java/com/chalange/magalums/controller/NotificationController.java
@@ -4,7 +4,6 @@ import com.chalange.magalums.dto.SchedulerNotificationDto;
 import com.chalange.magalums.entity.Notification;
 import com.chalange.magalums.service.NotificationService;
 import lombok.AllArgsConstructor;
-import lombok.NoArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -16,15 +15,20 @@ public class NotificationController {
     private final NotificationService notificationService;
 
     @PostMapping
-    public ResponseEntity<Void> scheduleNotification(@RequestBody SchedulerNotificationDto dto){
+    public ResponseEntity<Void> scheduleNotification(@RequestBody SchedulerNotificationDto dto) {
         notificationService.scheduleNotification(dto);
         return ResponseEntity.accepted().build();
     }
 
     @GetMapping("/{notificationId}")
-    public ResponseEntity<Notification> getNotification(@PathVariable("notificationId") Long notificationId){
+    public ResponseEntity<Notification> getNotification(@PathVariable("notificationId") Long notificationId) {
         var notification = notificationService.findById(notificationId);
         return notification.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
     }
 
+    @DeleteMapping("/{notificationId}")
+    public ResponseEntity<Void> cancelNotification(@PathVariable("notificationId") Long notificationId){
+        notificationService.cancelNotification(notificationId);
+        return  ResponseEntity.noContent().build();
+    }
 }
--- src/main/java/com/chalange/magalums/service/NotificationService.java
+++ src/main/java/com/chalange/magalums/service/NotificationService.java
@@ -2,6 +2,7 @@ package com.chalange.magalums.service;
 
 import com.chalange.magalums.dto.SchedulerNotificationDto;
 import com.chalange.magalums.entity.Notification;
+import com.chalange.magalums.entity.Status;
 import com.chalange.magalums.repository.NotificationRepository;
 import lombok.AllArgsConstructor;
 import org.aspectj.apache.bcel.classfile.Module;
@@ -25,6 +26,16 @@ public class NotificationService {
 
     public Optional<Notification> findById(Long notificationId){
         return notificationRepository.findById(notificationId);
+    }
+
+    public void cancelNotification(Long notificationId){
+        var notification = findById(notificationId);
+
+        if (notification.isPresent()){
+            notification.get().setStatus(Status.Values.CANCELLED.toStatus());
+            notificationRepository.save(notification.get());
+
+        }
     }
 
 }
