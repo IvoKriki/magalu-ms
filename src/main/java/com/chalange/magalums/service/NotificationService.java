c9eda7e440921721a18292072609115a55d8402e
Ivo Kriki <ivo.kriki@outlook.com>
2024-07-11 01:08:56 +0100

    created getNotification

--- src/main/java/com/chalange/magalums/service/NotificationService.java
+++ src/main/java/com/chalange/magalums/service/NotificationService.java
@@ -1,18 +1,30 @@
 package com.chalange.magalums.service;
 
 import com.chalange.magalums.dto.SchedulerNotificationDto;
+import com.chalange.magalums.entity.Notification;
 import com.chalange.magalums.repository.NotificationRepository;
 import lombok.AllArgsConstructor;
+import org.aspectj.apache.bcel.classfile.Module;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @Service
-@AllArgsConstructor
+
 public class NotificationService {
 
     private final NotificationRepository notificationRepository;
 
+    public NotificationService(NotificationRepository notificationRepository) {
+        this.notificationRepository = notificationRepository;
+    }
+
     public void scheduleNotification(SchedulerNotificationDto dto){
         notificationRepository.save(dto.toNotification());
+    }
+
+    public Optional<Notification> findById(Long notificationId){
+        return notificationRepository.findById(notificationId);
     }
 
 }
